d5db65c5bc88e10131cad3aa1853bafaaa3ed3d3
jzacka1 <jameszacka@gmail.com>
2019-04-05 02:00:30 -0400

    Add project files.

---
+++ JamesZacka_GoDataFeed_Coding_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JamesZacka_GoDataFeed_Coding_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
